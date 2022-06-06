7a3fa30601dae4b6c4d27eade8dd6b57049c7ac1
zuramai <saugi.dev@gmail.com>
2022-06-06 10:11:52 +0700

    add: role permission speculative generality

---
+++ before/User/Role/RolePermission.java
@@ -0,0 +1,14 @@
+package User.Role;
+
+public class RolePermission { 
+    // RolePermission is a feature to be added in future. Unimplemented.
+    public void hasPermission(Roles role, String permissionType) {
+
+    }
+    public void removePermission(Roles role, String permissionType) {
+
+    }
+    public void addPermission(Roles role, String permissionType) {
+
+    }
+}
